5277a8a3037e6734c6065d7108770c183a454cc0
KayRago <kayleighrago@gmail.com>
2019-09-26 13:53:46 -0700

    deleting an item from the UI and updating the percentages

--- starter/app.js
+++ starter/app.js
@@ -5,6 +5,20 @@ var budgetController = (function()
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+    
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if(totalIncome > 0) {
+             this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }
+       
+    };
+    
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
     };
     
     var Income = function(id, description, value) {
@@ -93,6 +107,19 @@ var budgetController = (function()
             
         },
         
+        calculatePercentages: function() {
+            data.allItems.exp.forEach(function(cur) {
+               cur.calcPercentage(data.totals.inc); 
+            });
+        },
+        
+        getPercentages: function() {
+            var allPerc = data.allItems.exp.map(function(cur) {
+               return cur.getPercentage();
+            });
+            return allPerc;
+        },
+        
         getBudget: function() {
             return {
                 budget: data.budget,
@@ -123,7 +150,8 @@ var UIController = (function()
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
     }
     
    return {
@@ -159,6 +187,11 @@ var UIController = (function()
            
        },
        
+       deleteListItem: function(selectorID) {
+           var el = document.getElementById(selectorID);
+           el.parentNode.removeChild(el);
+       },
+       
        clearFields: function() {
            var fields, fieldsArray;
            
@@ -184,6 +217,25 @@ var UIController = (function()
            } else {
                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
            }
+       },
+       
+       displayPercentages: function(percentages) {
+           var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+           
+           var nodeListForEach = function(list, callback) {
+               for (var i = 0; i < list.length; i++) {
+                   callback(list[i], i);
+               }
+           };
+           
+           nodeListForEach(fields, function(current, index) {
+               if (percentages[index] > 0) {
+                   current.textContent = percentages[index] + '%';
+               } else {
+                   current.textContent = '---';
+               }
+              
+           });
        },
        
        getDOMstrings: function() {
@@ -223,6 +275,17 @@ var controller = (function(budgetCtrl, UICtrl)
         UICtrl.displayBudget(budget);
     };
     
+    var updatePercentages = function() {
+        // 1. calculate percentages
+        budgetCtrl.calculatePercentages();
+        
+        // 2. read percentages from the budget controller
+        var percentages = budgetCtrl.getPercentages();
+        
+        // 3. update the UI with the new percentages
+        UICtrl.displayPercentages(percentages);
+    };
+    
     var ctrlAddItem = function() {
         var input, newItem;
         
@@ -241,6 +304,9 @@ var controller = (function(budgetCtrl, UICtrl)
         
             // 5. calculate and update budget
             updateBudget();
+            
+            // 6. calculate and update percentages
+            updatePercentages();
         }
 
     };
@@ -259,8 +325,13 @@ var controller = (function(budgetCtrl, UICtrl)
             budgetCtrl.deleteItem(type, ID);
             
             // 2. delete the item from the UI
+            UICtrl.deleteListItem(itemID);
             
             // 3. update to show the new budget
+            updateBudget();
+            
+            // 4. calculate and update percentages
+            updatePercentages();
         }
     };
     
